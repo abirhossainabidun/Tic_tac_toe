ee1508aa48e5126a58a9416ce14142706640bdbe
abirhossainabidun <48399413+abirhossainabidun@users.noreply.github.com>
2022-08-19 21:08:10 +0600

    Add files via upload

---
+++ Tic_tac_toe/src/main/java/com/example/aoop/GameController.java
@@ -0,0 +1,141 @@
+package com.example.aoop;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+    @FXML
+    private Text winnerText;
+
+    @FXML
+    public Button closeButton;
+
+    private int playerTurn = 0;
+
+    ArrayList<Button> buttons;
+
+    public void switchBack(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+        stage = ((Stage) ((Node) event.getSource()).getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchExit(ActionEvent event) throws IOException {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttons = new ArrayList<>(Arrays.asList(button1,button2,button3,button4,button5,button6,button7,button8,button9));
+
+        buttons.forEach(button ->{
+            setupButton(button);
+            button.setFocusTraversable(false);
+        });
+    }
+
+    @FXML
+    void restartGame(ActionEvent event) {
+        buttons.forEach(this::resetButton);
+        winnerText.setText("Tic-Tac-Toe");
+    }
+
+    public void resetButton(Button button){
+        button.setDisable(false);
+        button.setText("");
+    }
+
+    private void setupButton(Button button) {
+        button.setOnMouseClicked(mouseEvent -> {
+            setPlayerSymbol(button);
+            button.setDisable(true);
+            checkIfGameIsOver();
+        });
+    }
+
+    public void setPlayerSymbol(Button button){
+        if(playerTurn % 2 == 0){
+            button.setText("X");
+            playerTurn = 1;
+        } else{
+            button.setText("O");
+            playerTurn = 0;
+        }
+    }
+
+    public void checkIfGameIsOver(){
+        for (int a = 0; a < 8; a++) {
+            String line = switch (a) {
+                case 0 -> button1.getText() + button2.getText() + button3.getText();
+                case 1 -> button4.getText() + button5.getText() + button6.getText();
+                case 2 -> button7.getText() + button8.getText() + button9.getText();
+                case 3 -> button1.getText() + button5.getText() + button9.getText();
+                case 4 -> button3.getText() + button5.getText() + button7.getText();
+                case 5 -> button1.getText() + button4.getText() + button7.getText();
+                case 6 -> button2.getText() + button5.getText() + button8.getText();
+                case 7 -> button3.getText() + button6.getText() + button9.getText();
+                default -> null;
+            };
+
+            //X winner
+            if (line.equals("XXX")) {
+                winnerText.setText("X won!");
+            }
+
+            //O winner
+            else if (line.equals("OOO")) {
+                winnerText.setText("O won!");
+            }
+        }
+    }
+}
